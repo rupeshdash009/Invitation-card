87c56d70284b261c9a26309748a94bc56aa63375
rupeshdash009 <rupeshdash20@gmail.com>
2024-11-26 15:18:18 +0530

    1st commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('rsvpBtn').addEventListener('click', () => {
+    const rsvpMessage = `
+    Thank you for your interest in Computer Literacy Day! 
+    RSVP confirmation will be sent to your registered email.`;
+    alert(rsvpMessage);
+  });
+  
